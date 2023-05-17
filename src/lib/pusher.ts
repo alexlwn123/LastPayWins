ceed48e01e3382a427b92c4f3a0f00be449d3736
Alex Lewin <alex.lewin@blockspaces.com>
2023-05-17 14:31:34 -0400

    removed log

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,13 +1,5 @@
 import Pusher from "pusher";
 // import 'server-only';
-console.log('config',{
-  appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  secret: process.env.PUSHER_APP_SECRET!,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER!,
-  useTLS: true,
-} )
-
 const client = new Pusher({
   appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
