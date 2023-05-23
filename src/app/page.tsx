40f06cd3c8be0b65347fac562a9021adc197a5be
Alex Lewin <alex.lewin@blockspaces.com>
2023-05-22 22:59:18 -0400

    added favicon and spinner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import { CurrentWinner } from '@/components/CurrentWinner';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { fromSats } from 'satcomma';
+import { Triangle } from 'react-loader-spinner';
 
 export default function Home() {
   const [invoice, setInvoice] = useState(null);
@@ -22,13 +23,17 @@ export default function Home() {
   const [refetch, setRefetch] = useState(false)
   const [countdownKey, setCountdownKey] = useState<number>(0)
   const [fetching, setFetching] = useState(false);
+  const initialRender = useRef(true);
 
   const { lnAddress, timestamp, jackpot, status, timeLeft, setStatus } = usePusher();
 
   useEffect(() => {
     console.log('lnAddress', lnAddress, timestamp, jackpot, 'status-', status, 'timeleft-', timeLeft);
-    if (status === 'LIVE' && lnAddress !== userAddress) {
-      toast(`New Bid Received!\nCurrent Winner: ${lnAddress}`, { type: 'info' });
+    if (initialRender.current) {
+      initialRender.current = false;
+      return;
+    } else if (status === 'LIVE' && lnAddress !== userAddress) {
+      toast(`Bid Received! - ${lnAddress}`, { type: 'info' });
     } else if (status === 'EXPIRED' && lnAddress !== userAddress) {
       toast(`Timer Expired! ${lnAddress} wins ₿ ${fromSats(jackpot)}!`, { type: 'info' });
     }
@@ -65,8 +70,8 @@ export default function Home() {
 
   // Check invoice
   useEffect(() => {
-    console.log(settled, hash, status)
     if (settled || !hash || status === 'LOADING') return;
+    console.log(settled, hash, status)
     const interval = setInterval(() => {
       const isNew = status !== 'LIVE'
       const url = `/api/invoice?hash=${encodeURIComponent(hash!)}&lnaddr=${userAddress}&new=${isNew}`
@@ -106,53 +111,75 @@ export default function Home() {
         hideProgressBar={true}
         autoClose={3000}
         pauseOnFocusLoss={false}
-        theme='dark'
+        theme="dark"
         closeButton={false}
       />
       <div className={styles.description}>
         <h1>Last Pay Wins</h1>
-        <h2>Pay the invoice to {status === 'WAITING' ? 'start' : 'reset'} the timer. </h2>
+        <h2>
+          Pay the invoice to {status === "WAITING" ? "start" : "reset"} the
+          timer.{" "}
+        </h2>
         <h2>
           If the timer hits zero before someone else pays, you win the jackpot.
         </h2>
       </div>
-      <Jackpot jackpotSats={jackpot || 0} />
-      {/* <button onClick={() => setCountdownKey(prevKey => prevKey + 1)}>Reset timer</button> */}
-        <div className={styles.center}>
+      {status !== "LOADING" ? ( <Jackpot jackpotSats={jackpot || 0} />) : (
+        <Triangle
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="triangle-loading"
+          wrapperStyle={{}}
+          visible={true}
+        />
+      )}
+      <div className={styles.center}>
         {/* QR CODE */}
         <div className={styles.stack}>
-          {status !== 'LOADING' &&
+          {status !== "LOADING" && (
             <>
-              <Countdown currentTime={timeLeft} countdownKey={countdownKey} status={status} setStatus={setStatus}/>
-              <CurrentWinner currentWinner={lnAddress ?? 'Anon'} isActive={status === 'LIVE'} jackpot={jackpot} status={status} />
-            </>
-          }
+              <Countdown
+                currentTime={timeLeft}
+                countdownKey={countdownKey}
+                status={status}
+                setStatus={setStatus}
+              />
+              <CurrentWinner
+                currentWinner={lnAddress ?? "Anon"}
+                isActive={status === "LIVE"}
+                jackpot={jackpot}
+                status={status}
+              />
 
-          <Input
-            placeholder={"Lightning Address"}
-            onChange={(e) => setUserAddress(e.target.value)}
-            value={userAddress}
-            />
+              <Input
+                placeholder={"Lightning Address"}
+                onChange={(e) => setUserAddress(e.target.value)}
+                value={userAddress}
+              />
+            </>
+          )}
         </div>
-        {invoice && (
+        {status !== "LOADING" && (
           <div className={styles.payment}>
             <Qr invoice={invoice} />
             <button
               className={styles.copy}
               onClick={() =>
-                navigator?.clipboard?.writeText(invoice) ?? console.error('Failed to copy')
+                navigator?.clipboard?.writeText(invoice) ??
+                console.error("Failed to copy")
               }
             >
               Copy Invoice
             </button>
-            { weblnAvailable &&
+            {weblnAvailable && (
               <button
                 className={styles.copy}
                 onClick={() => handleWeblnPay(invoice)}
               >
                 Pay with WebLN
               </button>
-            }
+            )}
           </div>
         )}
       </div>
