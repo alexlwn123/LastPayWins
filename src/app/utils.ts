132dda6ef555769e9849e279b6edf9c5f49870c4
Alex Lewin <alex.lewin@blockspaces.com>
2023-05-31 18:05:51 -0400

    hotfix/validation-error

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -4,7 +4,7 @@ export const validateLnurl = async (lnurl: string) => {
   const url = `/api/validate?lnurl=${encodeURIComponent(lnurl)}`;
   const res = await fetch(url, { method: 'GET', });
   const data = await res.json();
-  if (res.status === 200 && data.status === 'OK') {
+  if (res.status === 200 && data.domain) {
     return { valid: true, domain: data.domain };
   } else {
     return { valid: false };
